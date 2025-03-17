1ce3da851a44d93e893507cc6f5285159e14b3ea
OlegRakovich <devgeslot@gmail.com>
2025-03-16 22:33:41 -0400

    Tests pre-steps

--- code/TestServer.AWS/LambdaEntryPoint.cs
+++ code/TestServer.AWS/LambdaEntryPoint.cs
@@ -1,5 +1,8 @@
-﻿using Amazon.Lambda.Core;
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 using Staticsoft.Asp.Lambda;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -9,5 +12,20 @@ public class LambdaEntryPoint : LambdaEntrypoint<AWSStartup>
 {
     protected override TriggerCollection Triggers => base.Triggers
         .AddTrigger<CloudWatchTrigger>()
-        .AddTrigger<RequestTrigger>();
+        .AddTrigger<RequestTrigger>()
+        .AddTrigger<WebSocketTrigger>();
+}
+
+public class WebSocketTrigger : TriggerSource
+{
+    public bool TryConvert(
+        JsonElement request,
+        JsonSerializerOptions options,
+        [NotNullWhen(true)] out APIGatewayProxyRequest? proxyRequest
+    )
+    {
+        Console.WriteLine(request.GetRawText());
+        proxyRequest = null;
+        return false;
+    }
 }
---
+++ code/WebSocketCommunication.Tests/WebSocketTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.WebSockets;
+
+namespace Staticsoft.WebSocketCommunication.Tests;
+
+public class WebSocketTests
+{
+    [Fact]
+    public async Task CanConnectAndDisconnect()
+    {
+        var client = new ClientWebSocket();
+
+    }
+}
